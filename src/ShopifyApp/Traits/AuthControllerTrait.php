f02480f232c2d03327b8d2a155e18e9217a23dbd
kalitk0 <gatevv@gmail.com>
2024-08-13 00:44:13 +0300

    Update AuthControllerTrait.php

--- src/ShopifyApp/Traits/AuthControllerTrait.php
+++ src/ShopifyApp/Traits/AuthControllerTrait.php
@@ -45,8 +45,7 @@ trait AuthControllerTrait
 
         // Start the process
         $auth = new AuthShopHandler($shop);
-        $session = new ShopSession($shop);
-        $session->setDomain($shopDomain);
+        
 
         if (!$request->filled('code')) {
             // Handle a request without a code, do a fullpage redirect
@@ -66,8 +65,10 @@ trait AuthControllerTrait
 
         // We have a good code, get the access details
         $access = $auth->getAccess($validated['code']);
+        $session = new ShopSession($shop);
+        $session->setDomain($shopDomain);
         $session->setAccess($access);
-
+        
         // Do post processing and dispatch the jobs
         $auth->postProcess();
         $auth->dispatchJobs();
